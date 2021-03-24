e64c1f13ab91151b3b4be67cd8b3423676a99401
sinonodd <kamal8aly545@gmail.com>
2021-03-24 14:38:24 +0100

    :sparkles: added shoppig carte

---
+++ client/src/components/ShoppingCart.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="text-right d-block">
+    <button @click="show()" class="btn btn-success">Cart
+      <span class="badge badge-light badge-pill">{{cart.length}}</span>
+    </button>
+    <div v-if="active" class="mb-4">
+      <div class="row mb-2" v-for="item in cart" :key="item._id">
+        <img  width="90" :src="item.image" alt="itemImage">
+        <h6 class="m-auto text-secondary">{{item.name}}</h6>
+        <h6 class="m-auto text-danger">{{item.price}} MAD</h6>
+        <h6 class="m-auto">{{item.category}}</h6>
+        <button @click="removeItem(item)" class="btn btn-danger my-auto">Remove</button>
+      </div>
+      <button v-if="!empty" @click="checkout()" class="btn btn-warning">Checkout</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ['products', 'cart', 'empty'],
+  data: () => ({
+    active: false,
+  }),
+  methods: {
+    show() {
+      this.active = !this.active;
+    },
+    checkout() {
+      console.log('/checkout');
+    },
+    removeItem(item) {
+      this.cart.splice(this.cart.indexOf(item), 1);
+      if (this.cart.length === 0) {
+        window.location.reload();
+      }
+    },
+  },
+};
+</script>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,25 +1,41 @@
 <template>
-  <div class="home row">
-    <div v-for="product in products" :key="product._id" class="col-md-4 playlist">
-        <img :src="product.image" class="img-thumbnail">
-        <h4>{{product.name}}</h4>
-        <span class="text-muted">{{product.price}}$</span>
-    </div>
+  <div>
+    <ShoppingCart :empty="empty" :products="products" :cart="cart"></ShoppingCart>
+    <div class="home row">
+      <div v-for="product in products" :key="product._id" class="col-md-4 playlist">
+          <img :src="product.image" class="img-thumbnail">
+          <h4>{{product.name}}</h4>
+          <span class="text-muted">{{product.price}}$</span>
+          <button @click="addToCart(product)" class="btn btn-dark">Add to cart</button>
       </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import ShoppingCart from '@/components/ShoppingCart.vue';
 
 const API_URL = 'http://localhost:5000/';
 export default {
+  components: {
+    ShoppingCart,
+  },
   data: () => ({
+    empty: true,
     id: '',
     products: [],
+    cart: [],
   }),
   async mounted() {
     const res = await fetch(API_URL);
     const result = await res.json();
     this.products = result;
+  },
+  methods: {
+    addToCart(item) {
+      this.cart.push(item);
+      this.empty = false;
+    },
   },
 };
 </script>
