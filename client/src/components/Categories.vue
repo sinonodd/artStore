e57a675b581a3c86cf2c88a80b5353054dc8791a
sinonodd <kamal8aly545@gmail.com>
2021-04-06 21:51:53 +0100

    :sparkles: Added brand and made categories dinamic and ready for filtering

--- client/src/components/Categories.vue
+++ client/src/components/Categories.vue
@@ -1,12 +1,24 @@
 <template>
-  <div class="categories btn-block btn-group-vertical">
-    <button type="button" class="text-light btn btn-info">Categories</button>
-    <button type="button" class="text-light btn btn-info">Human</button>
-    <button type="button" class="text-light btn btn-info">Draw</button>
-    <button type="button" class="text-light btn btn-info">Wallpaper</button>
-    <button type="button" class="text-light btn btn-info">dark and white</button>
-    <button type="button" class="text-light btn btn-info">Abstract</button>
+  <section class="text-left">
+  <h4 class="text-muted categories pb-1 my-4 border-bottom">Categories</h4>
+  <div v-for="product in products" :key="product.category" class="btn-block btn-group-vertical">
+    <router-link
+      class="text-muted mb-3 pb-1 border-bottom" to="#">{{product.category}}</router-link>
   </div>
+  </section>
 </template>
+<script>
+export default {
+  props: ['products'],
+  date: () => ({
+    categories: [],
+  }),
+};
+</script>
 <style>
+.categories {
+  color: #454541;
+}
+.categories-container {
+}
 </style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,17 +2,19 @@
   <div class="product-card">
     <img :src="background" width="100%" alt="hero">
     <ShoppingCart :empty="empty" :products="products" :cart="cart"></ShoppingCart>
-    <div class="home row ml-auto mr-auto container">
-      <Categories class="categories col-md-2"></Categories>
-      <div class="ml-auto row col-md-10">
-        <div v-for="product in products" :key="product._id" class="mt-4 col-md-3 mx-auto">
+    <div class="featured-products row ml-auto mr-auto container">
+      <Categories :products="products" class="categories col-md-2"></Categories>
+
+      <section class="ml-auto row col-md-10">
+        <div v-for="product in products" :key="product._id" class="mt-4 col-md-4 mx-auto">
           <div class="card bg-light shadow-sm rounded">
             <router-link :to="{name: 'Product', params: {id: product._id}}" class="m-0">
               <img :src="product.image" width="100%">
             </router-link>
               <div class="d-flex justify-content-between">
-                <div class="my-1 d-flex flex-column">
-                  <h6 class="text-secondary text-left ml-1">{{product.name}}</h6>
+                <div class="my-1 ml-1 d-flex flex-column">
+                  <h5 class="text-muted text-left ml-1">{{product.name}}</h5>
+                  <h6 class="text-muted text-left ml-1">{{product.category}}</h6>
                   <span class="text-info d-block text-left ml-1">{{product.price}}$</span>
                 </div>
                 <div class="">
@@ -21,13 +23,14 @@
                     <svg class="add mr-1" fill="#454541" width="18" enable-background="new 0 0 455 455" version="1.1" viewBox="0 0 455 455" xml:space="preserve" xmlns="http://www.w3.org/2000/svg">
                       <path d="m0 0v455h455v-455h-455zm358.86 242.64h-116.5v116.5h-30v-116.5h-116.5v-30h116.5v-116.5h30v116.5h116.5v30z"/>
                     </svg>
-                    <!-- eslint-disable -->
+                    <!-- eslint-enable -->
                   </button>
                 </div>
               </div>
           </div>
         </div>
-      </div>
+      </section>
+
     </div>
       </div>
 </template>
@@ -75,7 +78,7 @@ export default {
 };
 </script>
 <style>
-.home {
+.featured-products {
   background: #f2f3f2;
 }
 .card {
