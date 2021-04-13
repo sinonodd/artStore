944fa278311c17729646a19c96350ff35b3813e8
sinonodd <kamal8aly545@gmail.com>
2021-04-13 11:18:55 +0000

    :sparkles: Added checkbox for categories

--- client/src/components/Categories.vue
+++ client/src/components/Categories.vue
@@ -2,9 +2,14 @@
   <section class="text-left">
   <h4 class="text-muted categories pb-1 my-4 border-bottom">Categories</h4>
   <div v-for="product in products" :key="product.category" class="btn-block btn-group-vertical">
-    <router-link @click.native="filter(product.category)"
-      class="text-muted mb-3 pb-1 border-bottom" to="#">{{product.category}}
-    </router-link>
+    <div class="form-check">
+      <input
+        @change="filter(product.category)"
+        class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+        <label class="form-check-label" for="flexCheckDefault">
+          {{product.category}}
+        </label>
+    </div>
   </div>
   </section>
 </template>
